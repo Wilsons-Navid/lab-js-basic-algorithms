654a02070db5ce720fd28eccc3cf3c580761042e
Wilsons-Navid <wadotiwawil@gmail.com>
2023-10-21 16:00:48 -0700

    done with first iteration

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 // Iteration 1: Names and Input
+let hacker1 = 'John'
+console.log(`the driver's name is ${hacker1}`)
+
+let hacker2 = 'Peter'
+
+console.log(`The navigator's name is ${hacker2}`)
+
+
 //
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
+
+console.log('i am ready')
