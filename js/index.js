0b455a27164d97c4efe16643dca715783a37370b
Wilsons-Navid <wadotiwawil@gmail.com>
2023-10-22 04:11:52 -0700

    done with iteration 3

--- js/index.js
+++ js/index.js
@@ -20,8 +20,29 @@ else if(lengthOfNavigatorsName > lengthOfDriverName){
     console.log(`It seems that the navigator has the longest name, it has ${lengthOfNavigatorsName} characters`)
 }
 else if(lengthOfDriverName === lengthOfNavigatorsName){
-    console.log(`Wow, you both have equally long names, ${lengthNavigatorName} characters!`)
+    console.log(`Wow, you both have equally long names, ${lengthOfNavigatorsName} characters!`)
 }
+
 // Iteration 3: Loops
 
+console.log(hacker1.split("").join(" ").toUpperCase())
+console.log(hacker2.split("").reverse("").join(""))
+
+
+if (hacker1 > hacker2){
+    console.log('The driver\'s name goes first')
+}
+
+else if(hacker2 > hacker1){
+    console.log('Yo, the navigator goes first definitely')
+}
+
+else  if(hacker1 === hacker2){
+    console/log('What? You both have the same name')
+}
+
+
+
 console.log('i am ready')
+
+
